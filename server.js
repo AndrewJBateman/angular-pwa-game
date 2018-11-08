0081e781aa5835bd619986eec2173e40a444f16d
Andrew Bateman <gomezbateman@yahoo.com>
2018-11-08 11:34:02 +0100

    update to server.js & package.json

--- server.js
+++ server.js
@@ -7,9 +7,8 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/angular7-app-api-news'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/angular7-app-api-news/index.html'));
+app.get('/*', function(req,res) { 
+	res.sendFile(path.join(__dirname+'/dist/angular7-app-api-news/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
