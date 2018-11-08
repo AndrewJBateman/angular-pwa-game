81628c8658df3af1be6de9b74a8fe274764b4978
Andrew Bateman <gomezbateman@yahoo.com>
2018-11-08 12:10:49 +0100

    update to server.js & package.json

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-//Install express server
 const express = require('express');
-const path = require('path');
-
 const app = express();
+const path = require('path');
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular7-app-api-news'));
-
-app.get('/*', function(req,res) { 
-	res.sendFile(path.join(__dirname+'/dist/angular7-app-api-news/index.html'));
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
+// Start the app by listening on the default
+// Heroku port
 app.listen(process.env.PORT || 8080);
